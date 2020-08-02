02c64461fe5ffe5d4aee5a3bd78ad23c2a454454
tconsta <tulupov.kv@gmail.com>
2020-08-02 17:01:08 +0700

    New features in server.py

--- server/server.py
+++ server/server.py
@@ -13,15 +13,30 @@ SENSOR_DATA1 = 3    # temperature
 SENSOR_DATA2 = 4    # humidity (pi-4b)
 RELAY_STATE = 5     # state 'on'/'off' (pi-4b)
 
+URLS = [
+    '/',
+    '/fan/on',
+    '/fan/off',
+    '/fan/auto',
+    '/log/pi4b',
+    '/log/pizero']
+
 LOG_FILE_PI_4B = 'pi-4b.csv'
 LOG_FILE_PI_ZERO_W = 'pi-zero-w.csv'
 
+VALUE_FILE_PI_4B = 'pi-4b_value.csv'
+VALUE_FILE_PI_ZERO_W = 'pi-zero-w_value.csv'
+
 selector = selectors.DefaultSelector()
 
-turn_on_relay = True
+FAN_THRESHOLD = '27'     # Fan switch-on/off threshold temperature
+
+fan_client_socket = None
+fan_state = 'off_'
 
 
 def pi_server_config():
+
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind((SERVER_IP, SERVER_PI_PORT))
@@ -31,6 +46,7 @@ def pi_server_config():
 
 
 def http_server_config():
+
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind((SERVER_IP, SERVER_HTTP_PORT))
@@ -40,6 +56,7 @@ def http_server_config():
 
 
 def accept_pi_connection(server_socket):
+
     client_socket, addr = server_socket.accept()
     print('Connection from', addr)
     server_socket.setblocking(False)
@@ -47,6 +64,7 @@ def accept_pi_connection(server_socket):
 
 
 def accept_http_connection(server_socket):
+
     client_socket, addr = server_socket.accept()
     print('Connection from', addr)
     server_socket.setblocking(False)
@@ -64,31 +82,22 @@ def handle_pi_request(client_socket):
     save_log(data)
 
     if data[HOST] == 'pi-4b':
-        relay_control(client_socket, data[RELAY_STATE])
+        global fan_client_socket
+        fan_client_socket = client_socket
 
     if not request:
         selector.unregister(client_socket)
         client_socket.close()
 
 
-def relay_control(client_socket, relay):
-
-    if not turn_on_relay:
-        if relay == 'on':
-            # turn off relay
-            client_socket.send('turn off relay'.encode())
-            print('command sent: turn off relay')
-    else:
-        if relay == 'off':
-            # turn on relay
-            client_socket.send('turn on relay'.encode())
-            print('command sent: turn on relay')
-
-
 def save_log(data):
 
     if data[HOST] == 'pi-4b':
         with open(LOG_FILE_PI_4B, mode='a') as csv_file:
+            writer = csv.writer(csv_file, delimiter=',')
+            writer.writerow(data[1:])
+        # to get the last value (log file can be huge and take a long time to read)
+        with open(VALUE_FILE_PI_4B, mode='w') as csv_file:
             writer = csv.writer(csv_file, delimiter=',')
             writer.writerow(data[1:])
 
@@ -104,19 +113,104 @@ def save_log(data):
             writer = csv.writer(csv_file, delimiter=',')
             writer.writerow(data[1:])
 
+        with open(VALUE_FILE_PI_ZERO_W, mode='w') as csv_file:
+            writer = csv.writer(csv_file, delimiter=',')
+            writer.writerow(data[1:])
+
         temperature = data[SENSOR_DATA1]
 
         print(f'saved to {LOG_FILE_PI_ZERO_W}:')
         print(f'temperature = {temperature}')
 
 
+def replace_value(text, html_id, shift, value):
+
+    start = text.find(html_id)  # element id
+    text = text[:(start + shift)] + value + text[(start + shift + len(value)):]
+    return text
+
+
+def index_view(url):
+
+    print('URL:', url)
+    with open('index.html') as index_file:
+        index_string = index_file.read()
+
+    with open(VALUE_FILE_PI_4B) as values_file:
+        values = values_file.read().split(',')
+    pi4b_temperature = values[2]
+    pi4b_humidity = values[3]
+
+    with open(VALUE_FILE_PI_ZERO_W) as values_file:
+        values = values_file.read().split(',')
+    pizero_temperature = values[2].strip('\n')
+
+    shift = 24  # = len('pi4b-temp">Temperature: ')
+    index_string = replace_value(index_string, 'pi4b-temp', shift, pi4b_temperature)
+
+    shift = 22  # = len('pi4b-humid">Humidity: ')
+    index_string = replace_value(index_string, 'pi4b-humid', shift, pi4b_humidity)
+
+    shift = 26  # = len('pizero-temp">Temperature: ')
+    index_string = replace_value(index_string, 'pizero-temp', shift, pizero_temperature)
+
+    shift = 12  # = len('fanState = "')
+    index_string = replace_value(index_string, 'fanState', shift, fan_state)
+
+    return index_string
+
+
+def fan_control(url):
+
+    global fan_state
+    if url == '/fan/on':
+        fan_state = 'on__'
+        fan_client_socket.send('turn on fan'.encode())
+        print('command sent: turn on fan')
+        return 'Fan on'
+    elif url == '/fan/off':
+        fan_state = 'off_'
+        fan_client_socket.send('turn off fan'.encode())
+        print('command sent: turn off fan')
+        return 'Fan off'
+    elif url == '/fan/auto':
+        fan_state = 'auto'
+        fan_client_socket.send(('auto fan,' + FAN_THRESHOLD).encode())
+        print('command sent: auto fan', FAN_THRESHOLD)
+        return 'Fan auto'
+
+
+def show_log(url):
+
+    if url == '/log/pi4b':
+        with open('pi-4b.csv') as log_file:
+            log_string = log_file.read()
+    else:
+        with open('pi-zero-w.csv') as log_file:
+            log_string = log_file.read()
+    return log_string
+
+
 def handle_http_request(client_socket):
 
     request = client_socket.recv(4096)
-    print(request)
-    # parse_http_request(request)
-    index_page = open('index.html')
-    response = 'HTTP/1.1 200 OK\n\n' + index_page.read()
+
+    parsed = request.decode('utf-8').split(' ')
+    url = parsed[1]
+
+    if url not in URLS:
+        response = 'HTTP/1.1 404 Not found\n\n'
+    else:
+        if url == URLS[0]:
+
+            response = index_view(url)
+        elif url == URLS[1] or url == URLS[2] or url == URLS[3]:
+            response = fan_control(url)
+        elif url == URLS[4] or url == URLS[5]:
+            response = show_log(url)
+
+        response = 'HTTP/1.1 200 OK\n\n' + response
+
     client_socket.sendall(response.encode())
     selector.unregister(client_socket)
     client_socket.close()
